f2ef75d31bca343a7c8cb5148030bf41b431d21f
Achim <39227403+Achim-Sommer@users.noreply.github.com>
2022-06-20 06:29:17 +0200

    Initial commit

---
+++ config.lua
@@ -0,0 +1,30 @@
+Config = {}
+Config.Locale = 'de'
+Config.IncludeCash = false -- DONT TOUCH!
+Config.IncludeWeapons = true -- TRUE or FALSE
+Config.IncludeAccounts = true -- TRUE or FALSE
+Config.ExcludeAccountsList = {"bank"} --  DONT TOUCH!
+Config.OpenControl = 289 -- Key for opening inventory. Edit html/js/config.js to change key for closing it.
+Config.CloseControl = 200
+
+-- List of item names that will close ui when used
+Config.CloseUiItems = {"phone", "weed_seed", "tunerchip", "fixkit", "scratch_ticket","medikit"} -- Add your own items here!
+
+Config.ShopBlipID = 52
+Config.LiquorBlipID = 93
+Config.YouToolBlipID = 402
+Config.PrisonShopBlipID = 52
+Config.WeedStoreBlipID = 140
+Config.WeaponShopBlipID = 110
+
+Config.ShopLength = 14
+Config.LiquorLength = 10
+Config.YouToolLength = 2
+Config.PrisonShopLength = 2
+
+Config.MarkerSize = {x = 1.5, y = 1.5, z = 1.5}
+Config.MarkerColor = {r = 0, g = 128, b = 255}
+Config.Color = 2
+Config.WeaponColor = 1
+
+Config.LicensePrice = 5000
---
+++ html/locales/nl.js
@@ -0,0 +1,5 @@
+var invLocale = new Object();
+invLocale.dropItem = "Fallen lassen";
+invLocale.useItem = "Benutzen";
+invLocale.giveItem = "Geben";
+invLocale.secondInventoryNotAvailable = "Das Sekundärinventar ist nicht verfügbar.";
